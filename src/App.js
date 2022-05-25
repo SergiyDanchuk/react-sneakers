ca6df03827c89e41a26cf5bd8463de350d819cd1
SergiyDanchuk <sergey.dacnhuk.p@gmail.com>
2022-05-25 21:55:32 +0300

    create card with component and use css modules

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import Card from './components/Card'
 import Header from './components/Header'
 import Drawer from './components/Drawer'
 
+const arr = [
+  {title: 'Чоловічі кросівки  Nike Blazer Mid Suede', price: 12999 , imageUrl: '/img/sneakers/1.jpg' },
+  {title: 'Чоловічі кросівки Nike Air Max 270', price: 4999, imageUrl: '/img/sneakers/2.jpg' },
+  {title: 'Чоловічі кросівки Nike Blazer Mid Suede', price: 15600, imageUrl: '/img/sneakers/3.jpg' },
+  {title: 'Кросівки Puma X Aka Boku Future Rider', price: 12999, imageUrl: '/img/sneakers/4.jpg' }
+]
+
 const App = () => {
   return  <div className="wrapper clear">
     <Drawer />
@@ -15,7 +22,15 @@ const App = () => {
         </div>
       </div>
       <div className="d-flex">
-        <Card />
+        {
+          arr.map( (obj) => (
+            <Card
+              title = { obj.title }
+              price = { obj.price }
+              imageUrl = { obj.imageUrl }
+            />
+          ))
+        }
       </div>
     </div>
   </div>;
